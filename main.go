784bce7430956f17aad522cb23d756798ed58fee
Malcolm <malcolmmorton77@gmail.com>
2022-12-11 15:57:14 -0600

    [main.go] modifed and clarified how to read in the file

--- main.go
+++ main.go
@@ -2,44 +2,50 @@ package main
 
 // Multiple packages can be imported together.
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 )
 
-
-
-func ReadWords(filename string) []string{
-	items := []string{}
-		//open the text file
+func ReadWords(filename string) string {
+	items := ""
+	//open the text file
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err.Error())
-		return items
+		return items //return an empty string to buffer after printing error
 	}
-		//close the file later
+
+	//close the file at the end of the function
 	defer file.Close()
 
-		//read in each file and split on the whitespace
+	//read in each file and split on the bytes
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanBytes)
 	for scanner.Scan() {
-		items = append(items, scanner.Text())
+		items += scanner.Text() // append the scanner text in bytes to items string
 	}
-	return items
+	return items // return the items string
 }
 
-//driver code
+// driver code
 func main() {
-		chars := []string{}
-		filename := "example.cpp"
-		//take in a text file with C++ code
-		chars = ReadWords(filename)
-		for i, s := range chars{
-			fmt.Println(i, s)
-		}
-		//handle the \n to convert to the character for a stack
-			//identify the \n
-			//push our identifying character on the stack
-			//continue the loop
+	//declare variables
+	chars := ""
+	filename := "example.cpp"
+
+	//take in a text file with C++ code
+	chars = ReadWords(filename)
+
+	//testing the printing of the C++ code
+	fmt.Println(chars)
+	var i = 0
+	for i < len(chars) {
+		fmt.Println(string(chars[i]))
+		i += 1
+	}
+	//handle the \n to convert to the character for a stack
+	//identify the \n
+	//push our identifying character on the stack
+	//continue the loop
 }
